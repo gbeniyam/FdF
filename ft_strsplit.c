408b238745f4cdfbb563ab43eb707983667ae25f
Gavriel Beniyaminov <gbeniyam@42.us.org>
2019-06-06 18:18:14 -0700

    done

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: gbeniyam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/02 09:46:28 by gbeniyam          #+#    #+#             */
-/*   Updated: 2019/06/06 09:57:26 by gbeniyam         ###   ########.fr       */
+/*   Updated: 2019/06/06 10:53:53 by gbeniyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,11 @@ char	**ft_strsplit(char const *s, char c)
 	size_t	i;
 	size_t	j;
 	size_t	k;
-	char	wordcount;
 	char	**words;
 
 	if (!s)
 		return (NULL);
-	wordcount = ft_wordcount(s, c);
-	if (!(words = (char **)malloc(sizeof(*words) * (wordcount + 1))))
+	if (!(words = (char **)malloc(sizeof(*words) * (ft_wordcount(s, c) + 1))))
 		return (NULL);
 	i = -1;
 	j = 0;
--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: gbeniyam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/16 13:49:44 by gbeniyam          #+#    #+#             */
-/*   Updated: 2019/05/30 20:18:46 by gbeniyam         ###   ########.fr       */
+/*   Updated: 2019/06/06 17:20:29 by gbeniyam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
-	unsigned int	end;
 	char			*s_ptr;
 	size_t			i;
 
 	i = -1;
-	end = start;
-	s_ptr = (char *)s;
 	if (!(s_ptr = ft_strnew(len)))
+		return (NULL);
+	if (!s)
 		return (NULL);
 	while (++i < len)
 		s_ptr[i] = s[start + i];
+	s_ptr[i] = '\0';
 	return (s_ptr);
 }
